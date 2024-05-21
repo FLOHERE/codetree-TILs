3f0656a10f98bd8e4c799ab31c507127c23ef3bb
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-05-21 14:33:43 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240521/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
