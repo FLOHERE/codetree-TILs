4ccbf7574cd8157e4aa4e358f346290f7b763fc9
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-05-21 14:20:17 +0900

    Title: 문자 변경하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240521/문자 변경하기/change-charater.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 'T';
+        System.out.print(a);
+    }
+    public static void sub(String[]args){
+        char a = 'C';
+    }
+}
