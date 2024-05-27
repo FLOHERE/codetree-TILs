2b3be40c2e17b08d3379c774cd14b90b5c825f23
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-05-27 12:39:42 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240527/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a;
+        a = nextDouble();
+        
+        double b;
+        b = nextDouble();
+
+        double c;
+        c = nextDouble();
+
+        System.out.printf("%.3f", a+b+c);
+    }
+}
