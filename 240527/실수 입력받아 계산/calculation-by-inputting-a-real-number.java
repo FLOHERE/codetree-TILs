132580ab438df505687e5e4e3856866b44017e3f
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-05-27 12:33:56 +0900

    Title: 실수 입력받아 계산, Time: 159ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240527/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double a;
+        a = sc.nextDouble();
+
+        double b;
+        b = sc.nextDouble();
+
+        System.out.printf("%.2f",a+b);
+
+    }
+}
