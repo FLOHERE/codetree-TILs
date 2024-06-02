5416a59672a58e3d33d9eb3255b914bf22da2257
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-06-02 14:39:53 +0900

    Title: 등급 매기기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240602/등급 매기기/ranking.java
+++ 240602/등급 매기기/ranking.java
@@ -6,13 +6,13 @@ public class Main {
         int score = sc.nextInt();
         if(score>=90){
             System.out.print("A");
-        }else if(score>=80){
+        }else if(score>=80 && score<90){
             System.out.print("B");
-        }else if(score>=70){
+        }else if(score>=70 && score<80){
             System.out.print("C");
-        }else if(score>=60){
+        }else if(score>=60 && score<70){
             System.out.print("D");
-        }else if(score<50){
+        }else if(score<60){
             System.out.print("F");
         }
     }
