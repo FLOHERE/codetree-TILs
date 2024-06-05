27ce5d5a16885a5885c18d508011f10a8d6a18d9
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-06-06 00:00:31 +0900

    Title: 숫자의 조건 여부 3, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240606/숫자의 조건 여부 3/number's-condition-3.java
+++ 240606/숫자의 조건 여부 3/number's-condition-3.java
@@ -8,7 +8,7 @@ public class Main {
         if(a%13==0 || a%19==0){
             System.out.print("True");
         }else{
-            System.out.print("False")
+            System.out.print("False");
         }
     }
 }
