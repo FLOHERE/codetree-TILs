06f15372de6b8483d9fd186167ed919ecb8b870a
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-06-09 16:34:00 +0900

    Title: 최댓값 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240609/최댓값 구하기/maximum-value.java
+++ 240609/최댓값 구하기/maximum-value.java
@@ -7,44 +7,18 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if(a>=b){
-            if(a>=c){
-                System.out.print(a);
-            }
-        }else if(a>=b){
-            if(a<=c){
-                System.out.print(c);
-            }else{
-                System.out.print(a);
-            }
+        int max = a; // Assume 'a' is the largest initially
+
+        if (b > max) {
+            max = b; // If 'b' is greater than current 'max', update 'max'
         }
         
-        if(b>=a){
-            if(b>=c){
-                System.out.print(b);
-            }
-        }else if(b>a){
-            if(b<=c){
-                System.out.print(c);
-            }else{
-                System.out.print(b);
-            }
-        }else{
-            System.out.print(a);
+        if (c > max) {
+            max = c; // If 'c' is greater than current 'max', update 'max'
         }
         
-        if(c>=a){
-            if(c>=b){
-                System.out.print(c);
-            }
-        }else if(c>=a){
-            if(c<=b){
-                System.out.print(b);
-            }else{
-                System.out.print(c);
-            }
-        }else{
-            System.out.print(a);
-        }
+        System.out.print(max); // Print the largest value
+
+        sc.close();
     }
 }
