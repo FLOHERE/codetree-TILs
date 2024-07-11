f9a568467aaefad0f28f130e30e2012bef15a274
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-07-11 13:19:40 +0900

    Title: 100점 도달하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240711/100점 도달하기/reach-100-point.java
+++ 240711/100점 도달하기/reach-100-point.java
@@ -6,21 +6,21 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 0; i<=n; i++){
-            if(n>=90){
+            if(n>=90 && n<=100){
                 System.out.print("A" + " ");
             }else if(n>=80 && n<90){
                 System.out.print("B" + " ");
             }else if(n>=70 && n<80){
                 System.out.print("C" + " ");
-            }else if(n>60 && n<70){
+            }else if(n>=60 && n<70){
                 System.out.print("D" + " ");
             }else if(n<60){
                 System.out.print("F" + " ");
             }
-            if(n>=100){
+            if(n>100){
                 break;
             }else{
-                n++;
+                n+=1;
             }
         }
         
