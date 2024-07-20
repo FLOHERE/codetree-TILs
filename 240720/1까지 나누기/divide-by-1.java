84fac56a9ec03c265fcd94787b25118dddb02974
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-07-20 12:09:11 +0900

    Title: 1까지 나누기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240720/1까지 나누기/divide-by-1.java
+++ 240720/1까지 나누기/divide-by-1.java
@@ -4,22 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int result;
+        int result = n;
         int cnt = 0;
 
-        for(int i = 1; i<=n; i++){
-            result = n/i;
-
-            if(result/i<=1){
-                if(result/i==0){
-                    cnt++;
-                    break;
-                }else if(result%i!=0){
-                    break;
-                }
+        for(int i = 1; i <= n; i++){
+            if(result / i <= 1){
+                cnt++;
+                break;
             }
+            result = result / i;
             cnt++;
         }
         System.out.print(cnt);
+        sc.close();
     }
 }
