7d9efbf09f922cb47b607247fcd5c8242378e254
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-07-22 19:14:47 +0900

    Title: 3n + 1 수열, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/3n + 1 수열/3n-plus-1-sequence.java
+++ 240722/3n + 1 수열/3n-plus-1-sequence.java
@@ -7,17 +7,20 @@ public class Main {
         int cnt = 0;
 
         while(true){
-            if(n%2==0){
-               n = n/2;
-            }else{
-                n = (n*3)+1;
-            }
-            cnt++;
-
             if(n==1){
                 System.out.print(cnt);
                 break;
             }
+            
+            if(n%2==0){
+                n = n/2;
+                cnt++;
+            }else if(n%2!=0){
+                n = (n*3)+1;
+                cnt++;
+            }
+        
         }
+        
     }
 }
