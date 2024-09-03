4a9458163b62b903492bf9d903a1e7e0cd553bad
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-09-03 21:29:36 +0900

    Title: 별표 출력하기 3, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/별표 출력하기 3/print-star-3.java
+++ 240903/별표 출력하기 3/print-star-3.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = n; i > 0; i--) {                //아래쪽 삼각형
-			for (int j = n; j > i; j-=1) {
+        for (int i = n; i > 0; i--) {
+			for (int j = n; j > i; j-=1) {  //처음에 n과 i의 값이 같으니까 생략
 				System.out.print("  ");
 			}
 			for (int j = 1; j <= i * 2 - 1; j++) {
