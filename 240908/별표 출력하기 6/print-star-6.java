bd2e3c51ec040ec94172c6ce0faee0006bc8fe96
jo_soyoung <126861289+FLOHERE@users.noreply.github.com>
2024-09-08 19:56:45 +0900

    Title: 별표 출력하기 6, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240908/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = n; i>0; i--){
+            for(int j = n; j>i; j--){
+                System.out.print("  ");
+            }
+
+            for(int j = n; j>(2*n)-(2*i)-n+1; j--){
+                System.out.print("* ");
+            }
+           
+            System.out.println();
+        }
+
+        for(int i = 0; i<n-1; i++){
+            for(int j = 0; j<=n-i-3; j++){
+                System.out.print("  ");
+            }
+
+            for(int j = 0; j<n+(2*i); j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
