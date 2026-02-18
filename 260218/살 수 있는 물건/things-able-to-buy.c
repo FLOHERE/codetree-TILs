5383660d8bbe65e97abc7073e23387465e29d547
CALA <126861289+FLOHERE@users.noreply.github.com>
2026-02-18 18:48:48 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260218/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n>=1000 && n<3000){
+        printf("mask");
+    }else if(n>=3000){
+        printf("book");
+    }else{
+        printf("no");
+    }
+    return 0;
+}
